7feaaf3d4da9e001870fb149bcc0de651e5ae41c
estagioUepg <166936682+estagioUepg@users.noreply.github.com>
2024-05-16 18:31:57 -0300

    Update servidorIMPORT.bat

--- Veyon/servidorIMPORT.bat
+++ Veyon/servidorIMPORT.bat
@@ -1,7 +1,7 @@
 
 cd /d %~dp0
-copy filet.txt "C:\Program Files\Veyon"
+copy Ips.txt "C:\Program Files\Veyon"
 set /P sala=Escreva o nome da sala: 
 cd "C:\Program Files\Veyon"
-veyon-cli networkobjects import filet.txt location %sala% regex "^NOME:(%%name%%:.*)\s+HOST:(%%host%%:.*)$"
+veyon-cli networkobjects import Ips.txt location %sala% regex "^NOME:(%%name%%:.*)\s+HOST:(%%host%%:.*)$"
 pause
